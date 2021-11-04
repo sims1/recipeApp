36559897802aac7941f4cdb9e1cd8e19aafda567
sims1 <hi.ling.he@gmail.com>
2021-11-04 16:20:22 -0400

    Add article example and tests
    https://spring.io/guides/tutorials/spring-boot-kotlin/
    up until "Exposing HTTP API"

--- src/main/kotlin/com/mo/recipe/app/RecipeApp/RecipeAppApplication.kt
+++ src/main/kotlin/com/mo/recipe/app/RecipeApp/RecipeAppApplication.kt
@@ -1,11 +1,37 @@
 package com.mo.recipe.app.RecipeApp
 
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
-class RecipeAppApplication
+@Configuration
+class RecipeAppApplication {
+
+	@Bean
+	fun databaseInitializer(
+		userRepository: UserRepository,
+		articleRepository: ArticleRepository
+	) = ApplicationRunner {
+
+		val smaldini = userRepository.save(User("smaldini", "Stéphane", "Maldini"))
+		articleRepository.save(Article(
+			title = "Reactor Bismuth is out",
+			headline = "Lorem ipsum",
+			content = "dolor sit amet",
+			author = smaldini
+		))
+		articleRepository.save(Article(
+			title = "Reactor Aluminium has landed",
+			headline = "Lorem ipsum",
+			content = "dolor sit amet",
+			author = smaldini
+		))
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<RecipeAppApplication>(*args) {
